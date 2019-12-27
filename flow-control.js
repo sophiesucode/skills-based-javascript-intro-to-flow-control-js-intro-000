48a4f21240326d4224a0d3ec1fcf6ae9764957fd
super-archive-1419 <sophzsul@gmail.com>
2019-12-27 01:03:46 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 function basicTeenager(age) {
-  if( age => 13 && age < 19 ){return('You are a teenager'!)
+  if( age => 13 && age < 19 ){return('You are a teenager!')
   else if (age<13) {return('You are a kid!')
 
 
